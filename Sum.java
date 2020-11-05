d0ac6044f784558941996f0fddffdf05f3a4d488
kirti1798 <71423039+kirti1798@users.noreply.github.com>
2020-11-05 13:46:32 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,31 @@
+
+class Sum {
+
+	boolean ak(int A[], int arr_size, int sum)
+	{
+		int l, r;
+		for (int i = 0; i < arr_size - 2; i++) 
+		{
+			for (int j = i + 1; j < arr_size - 1; j++) 
+			{
+				for (int k = j + 1; k < arr_size; k++) 
+				{
+					if (A[i] + A[j] + A[k] == sum) 
+					{
+						System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args)
+	{
+		Sum triplet = new Sum();
+		int A[] = { 1, 2, 6, 9, 3 };
+		int sum = 13;
+		int arr_size = A.length;
+		triplet.ak(A, arr_size, sum);
+	}
+}
